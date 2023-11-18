6d959bd2c461296e695823e16eed05461567c61d
Iqra khan <ik22082002khan@gmail.com>
2023-11-18 07:16:08 +0000

    stack and heap section

--- 01_basic/datatype-summry.js
+++ 01_basic/datatype-summry.js
@@ -34,3 +34,23 @@ const languages = [ "c" , "cpp" , "java" ,"python"];
  const newfuc = function(){
     let name2="muskan";
 }
+
+//***********stack and heap ************************* 
+//stack(primitive data type )   and heap(refrence datatype)
+
+//in stack the stack pass the copy of object which canot be change orignal value 
+
+let user1 = "muskan";
+let user2 =user1
+console.log(user2) //same as user1 
+
+user2 = "iqra";
+console.log(user2) //its diffrent from first one but  user1 value same as privious value 
+
+
+//heap (refrence type data type )
+let obj1 = {
+    name:"User1",
+    email:"123@gmail.com",
+}
+   let obj2 =obj1;// the both value are same but if we updated the ob2 vale it will also reflect from oure main obj1 
