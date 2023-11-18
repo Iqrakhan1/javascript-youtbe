d12b4fbf6d0b5a16785144081bc077a7dec9969a
Iqra khan <ik22082002khan@gmail.com>
2023-11-18 05:55:22 +0000

    variable file add

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -2,5 +2,8 @@ const name ="Iqra khan"
 let post = "intern"
 var number = 1234567890;
 city = "bhopal";
+/*
 
+perfre not use var  
+because of issu in block scope and function scope */
  console.table([name , post,number , city ]);
