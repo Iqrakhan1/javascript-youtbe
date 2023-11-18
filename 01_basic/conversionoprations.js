057dc03bedfd029292507ad54ce723f53c2dfac9
Iqra khan <ik22082002khan@gmail.com>
2023-11-18 05:55:41 +0000

    data conversion

---
+++ 01_basic/conversionoprations.js
@@ -0,0 +1,10 @@
+let name= "iqra";
+/*we can covert any string to number and number to string */
+const stringinnum = Number(name);
+console.log(typeof stringinnum);
+console.log( stringinnum);
+/* number to boolean */
+let number = 1;
+
+let numtobool = Boolean(number)
+console.log(numtobool);
