ba4489f82b05f8956646b7cc3ee09019cfba5cd1
Iqra khan <ik22082002khan@gmail.com>
2023-11-18 06:59:38 +0000

    datatype  with comment

---
+++ 01_basic/datatype-summry.js
@@ -0,0 +1,36 @@
+let value = 9;
+let ngValue= -value;
+// primitive datatypes
+
+//7 types  number , string , null, boolean , bigint , undefine , symbol
+
+let number=56;
+let string= "iqra";
+let nullVall=null; //its object type in typeof functon
+let boolean = true;
+let undefineValue = undefined;
+// its use n a the end of number to change it to big int number
+let bigint = 28283423840912849812093480938n;
+
+//symbole provide us to creat uniqe id number  if the value same in symbole but it treated as  a diffrent number 
+let id = symbol(5);
+let newid = symbol(5);
+
+
+//refrence type  
+// object , array , function 
+
+//array
+const languages = [ "c" , "cpp" , "java" ,"python"];
+ console.log(languages)
+
+  //object 
+
+  const newobj={
+    name :"iqra",
+    age:20,
+  }
+//function
+ const newfuc = function(){
+    let name2="muskan";
+}
